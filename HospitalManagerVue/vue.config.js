fdfe5b9cb1d72df4895149a251e5b090b76a43a6
yongpengzhou <112800206+bililateral@users.noreply.github.com>
2025-08-25 21:29:46 +0800

    Update vue.config.js

--- HospitalManagerVue/vue.config.js
+++ HospitalManagerVue/vue.config.js
@@ -6,10 +6,10 @@
 module.exports = {
     lintOnSave: false, // 关闭eslint校验
     devServer: {
-        host: "localhost",
-        port: 9282,
+        host: "localhost",    // 开发服务器运行的主机地址
+        port: 9282,           // 开发服务器端口（前端项目运行的端口）
+        proxy: "http://localhost:9281",  // 代理目标：后端API服务器地址
         https: false,
-        proxy: "http://localhost:9281",
         overlay: { // 关闭eslint校验
             warning: false,
             errors: false
