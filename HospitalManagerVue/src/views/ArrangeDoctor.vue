13eb07a941dcd6ee2751a34e73de4a5ca126e79b
yongpengzhou <112800206+bililateral@users.noreply.github.com>
2025-08-26 14:09:31 +0800

    Update ArrangeDoctor.vue

--- HospitalManagerVue/src/views/ArrangeDoctor.vue
+++ HospitalManagerVue/src/views/ArrangeDoctor.vue
@@ -2,88 +2,35 @@
     <div>
         <!-- 面包屑 -->
         <el-breadcrumb separator-class="el-icon-arrow-right">
-            <el-breadcrumb-item :to="{ path: '/sectionIndex' }"
-                >返回科室</el-breadcrumb-item
-            >
+            <el-breadcrumb-item :to="{ path: '/sectionIndex' }">返回科室</el-breadcrumb-item>
             <el-breadcrumb-item>{{ section }}</el-breadcrumb-item>
         </el-breadcrumb>
-        <el-input
-            v-model="query"
-            placeholder="请输入姓名查询"
-            class="doctorInput"
-        >
-            <el-button
-                slot="append"
-                icon="el-icon-search"
-                @click="requestDoctors"
-            ></el-button>
+        <el-input v-model="query" placeholder="请输入姓名查询" class="doctorInput">
+            <el-button slot="append" icon="el-icon-search" @click="requestDoctors"></el-button>
         </el-input>
         <el-table :data="doctorData" border>
-            <el-table-column
-                label="账号"
-                prop="dId"
-                v-model="doctorData.dId"
-            ></el-table-column>
-            <el-table-column
-                label="姓名"
-                prop="dName"
-                v-model="doctorData.dName"
-            ></el-table-column>
-            <el-table-column
-                label="性别"
-                prop="dGender"
-                v-model="doctorData.dGender"
-            ></el-table-column>
-            <el-table-column
-                label="职位"
-                prop="dPost"
-                v-model="doctorData.dPost"
-            ></el-table-column>
-            <el-table-column
-                label="部门"
-                prop="dSection"
-                v-model="doctorData.dSection"
-            ></el-table-column>
+            <el-table-column label="账号" prop="dId" v-model="doctorData.dId"></el-table-column>
+            <el-table-column label="姓名" prop="dName" v-model="doctorData.dName"></el-table-column>
+            <el-table-column label="性别" prop="dGender" v-model="doctorData.dGender"></el-table-column>
+            <el-table-column label="职位" prop="dPost" v-model="doctorData.dPost"></el-table-column>
+            <el-table-column label="部门" prop="dSection" v-model="doctorData.dSection"></el-table-column>
             <el-table-column label="操作" prop="dSection" width="180" fixed="right">
                 <template slot-scope="scope">
-                    <el-button
-                        v-if="scope.row.arrangeId == null"
-                        type="success"
-                        style="font-size: 18px"
-                        @click="arrangeClick(scope.row.dId)"
-                    >
-                        <i
-                            class="iconfont icon-r-yes"
-                            style="font-size: 22px"
-                        ></i>
-                        排班</el-button
-                    >
-                    <el-button
-                        v-if="scope.row.arrangeId != null"
-                        type="danger"
-                        style="font-size: 18px"
-                        @click="deleteArrange(scope.row.arrangeId)"
-                    >
-                        <i
-                            class="iconfont icon-r-delete"
-                            style="font-size: 22px"
-                        ></i>
-                        取消排班</el-button
-                    >
+                    <el-button v-if="scope.row.arrangeId == null" type="success" style="font-size: 18px"
+                        @click="arrangeClick(scope.row.dId)">
+                        <i class="iconfont icon-r-yes" style="font-size: 22px"></i>
+                        排班</el-button>
+                    <el-button v-if="scope.row.arrangeId != null" type="danger" style="font-size: 18px"
+                        @click="deleteArrange(scope.row.arrangeId)">
+                        <i class="iconfont icon-r-delete" style="font-size: 22px"></i>
+                        取消排班</el-button>
                 </template>
             </el-table-column>
         </el-table>
         <!-- 分页 -->
-        <el-pagination
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            background
-            layout="total, sizes, prev, pager, next, jumper"
-            :current-page="pageNumber"
-            :page-size="size"
-            :page-sizes="[1, 2, 4, 8, 16]"
-            :total="total"
-        >
+        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" background
+            layout="total, sizes, prev, pager, next, jumper" :current-page="pageNumber" :page-size="size"
+            :page-sizes="[1, 2, 4, 8, 16]" :total="total">
         </el-pagination>
     </div>
 </template>
@@ -183,6 +130,7 @@ export default {
 .el-breadcrumb {
     margin-bottom: 10px;
 }
+
 .doctorInput {
     width: 30%;
     margin-bottom: 10px;
